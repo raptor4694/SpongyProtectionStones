cd29186093c0f5a36b7d092ad0e7ee1e4c0b1d72
rodel77 <rodel1407@hotmail.com>
2017-12-31 23:58:55 -0600

    New Year!

---
+++ src/main/java/mx/com/rodel/sps/command/ICommand.java
@@ -0,0 +1,10 @@
+package mx.com.rodel.sps.command;
+
+public abstract class ICommand {
+	abstract void onCommand();
+	
+	   /////////////////////////
+ 	  // HAPPY NEW YEAR 2017 //
+   	 ////// From Mexico //////
+	/////////////////////////
+}
